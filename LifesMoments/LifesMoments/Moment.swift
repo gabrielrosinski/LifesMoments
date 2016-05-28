6a42014d20070714863d06f3d6375285c9955bd5
Gabriel <gabriel@Gabriels-MacBook-Pro.local>
2016-05-28 15:20:12 +0300

    - some moment fix

--- LifesMoments/LifesMoments/Moment.swift
+++ LifesMoments/LifesMoments/Moment.swift
@@ -62,6 +62,27 @@ class Moment: Object {
         
     }
     
+    convenience init(momentJsonStr: [String : AnyObject]) {
+        self.init()
+        
+        //date
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        self._date = dateFormatter.dateFromString((momentJsonStr["Date"] as? String)!)!
+        
+        //data
+        let dataStr = momentJsonStr["Data"] as? String
+        self._mediaData = (dataStr?.dataFromHexadecimalString()!)!
+        
+        self._latitude = Double((momentJsonStr["Latitude"] as? String)!)!
+        self._longitude = Double((momentJsonStr["Longitude"] as? String)!)!
+        self._momentID = Int((momentJsonStr["MomentID"] as? String)!)!
+        self._mediaType = Int((momentJsonStr["MediaType"] as? String)!)!
+        self._storyId = (momentJsonStr["StoryId"] as? String)!
+        
+    }
+    
+    
     required convenience init(coder decoder: NSCoder) {
         self.init()
         self._date = (decoder.decodeObjectForKey("_date") as? NSDate)!
@@ -92,11 +113,42 @@ class Moment: Object {
         }
         return "";
     }
+
     
-    ////    to use this init it must be initiliazde with params
-    //    convenience init(storyId:String){
-    //        self.init()
-    //        //self._name = name
-    //  
-    //    }
+}
+
+extension String {
+    
+    /// Create NSData from hexadecimal string representation
+    ///
+    /// This takes a hexadecimal representation and creates a NSData object. Note, if the string has any spaces, those are removed. Also if the string started with a '<' or ended with a '>', those are removed, too. This does no validation of the string to ensure it's a valid hexadecimal string
+    ///
+    /// The use of `strtoul` inspired by Martin R at http://stackoverflow.com/a/26284562/1271826
+    ///
+    /// - returns: NSData represented by this hexadecimal string. Returns nil if string contains characters outside the 0-9 and a-f range.
+    
+    func dataFromHexadecimalString() -> NSData? {
+        let trimmedString = self.stringByTrimmingCharactersInSet(NSCharacterSet(charactersInString: "<> ")).stringByReplacingOccurrencesOfString(" ", withString: "")
+        
+        // make sure the cleaned up string consists solely of hex digits, and that we have even number of them
+        
+        let regex = try! NSRegularExpression(pattern: "^[0-9a-f]*$", options: .CaseInsensitive)
+        
+        let found = regex.firstMatchInString(trimmedString, options: [], range: NSMakeRange(0, trimmedString.characters.count))
+        if found == nil || found?.range.location == NSNotFound || trimmedString.characters.count % 2 != 0 {
+            return nil
+        }
+        
+        // everything ok, so now let's build NSData
+        
+        let data = NSMutableData(capacity: trimmedString.characters.count / 2)
+        
+        for var index = trimmedString.startIndex; index < trimmedString.endIndex; index = index.successor().successor() {
+            let byteString = trimmedString.substringWithRange(Range<String.Index>(start: index, end: index.successor().successor()))
+            let num = UInt8(byteString.withCString { strtoul($0, nil, 16) })
+            data?.appendBytes([num] as [UInt8], length: 1)
+        }
+        
+        return data
+    }
 }
